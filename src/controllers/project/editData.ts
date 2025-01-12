71583feafe9ae2695ecc31127c18410ede85c3c5
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-12 19:11:47 +0530

    done the edit part

---
+++ src/controllers/project/editData.ts
@@ -0,0 +1,26 @@
+import asyncErrorHandler from "../../utils/asyncHandler";
+import { Request,Response,NextFunction } from "express";
+import { BadRequestError,ProjectNotFoundError  } from "../../utils/errorTypes";
+import ImageModel from "../../models/imageModel";
+import { StatusCodes } from "http-status-codes";
+
+
+export const editData = asyncErrorHandler(
+    async(req:Request,res:Response,next:NextFunction)=>{ 
+        console.log('editzzz');
+        
+        const { description, url ,id,userid} = req.body; 
+        if (!id) {
+            throw new BadRequestError("user id is required")
+        }
+        const updateItem = await ImageModel.findByIdAndUpdate(
+            id,
+            {description,url,userid},
+            { new: true, runValidators: true }
+        )
+      if(!updateItem){
+          throw new ProjectNotFoundError("Item is not found")
+      }
+       res.status(StatusCodes.OK).json({success:true,message:"data updated succesfully"})
+    }
+)
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,12 +2,13 @@ import express,{Router} from 'express'
 import { uploadImage } from '../controllers/project/uploadData'
 import { getData } from '../controllers/project/getData'
 import { deleteData } from '../controllers/project/deleteData'
+import { editData } from '../controllers/project/editData'
 
 const router:Router = express.Router()
 
 router.post('/upload',(req,res,next)=>uploadImage(req,res,next))
 router.get('/getData',(req,res,next)=>getData(req,res,next))
 router.delete('/deleteData/:id/:userid',(req,res,next)=>deleteData(req,res,next))
-
+router.put('/update',(req,res,next)=>editData(req,res,next))
 
 export default router
