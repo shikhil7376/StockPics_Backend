4a5dce0d59ae78838822cf28378a00ed58d9fdbc
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-13 00:59:52 +0530

    done the reset otp part

--- src/controllers/auth/resetPassword.ts
+++ src/controllers/auth/resetPassword.ts
@@ -6,11 +6,12 @@ import { generateVerificationCode } from "../../utils/codeGenerator";
 import verificationCode from "../../models/verificationCode";
 import { sendMail } from "../../utils/sendMails";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from 'bcrypt'
 
 
 export const ResetPasswordOtp = asyncErrorHandler(
     async(req:Request,res:Response,next:NextFunction)=>{
-        const {email} = req.body
+        const {email} = req.body    
         if(!email){
             throw new BadRequestError("Email is required")
         }
@@ -19,6 +20,7 @@ export const ResetPasswordOtp = asyncErrorHandler(
         throw new BadRequestError("user is not registered")
       }
       const code = generateVerificationCode()
+      await verificationCode.deleteMany({user:user._id})
       await verificationCode.create({
           user:user._id,
           code
@@ -28,3 +30,28 @@ export const ResetPasswordOtp = asyncErrorHandler(
     }
 )
 
+export const ResetPassword = asyncErrorHandler(
+    async(req:Request,res:Response,next:NextFunction)=>{
+       const{email,password} = req.body.data   
+       console.log(email,password);
+       
+      
+       
+       const SALT_ROUNDS = 10;
+       if(!email || !password){
+        throw new BadRequestError("both email and password is required")
+       }
+       const salt = await bcrypt.genSalt(SALT_ROUNDS)
+       const hashedPassword = await bcrypt.hash(password,salt)
+       const result = await User.updateOne(
+        {
+          email: email,
+        },
+        { $set: { password: hashedPassword } }
+      );
+    if(result.modifiedCount>0){
+        res.status(StatusCodes.OK).json({success:true,message:"password changed succesfully!!"})
+    }
+    
+    }
+)
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -3,6 +3,8 @@ import { registerUser } from '../controllers/auth/registerUser'
 import { verifyUser } from '../controllers/auth/verifyUser'
 import { loginUser } from '../controllers/auth/loginUser'
 import { resendOtp } from '../controllers/auth/resendOtp'
+import { ResetPasswordOtp,ResetPassword } from '../controllers/auth/resetPassword'
+
 
 const router:Router = express.Router()
 
@@ -10,5 +12,7 @@ router.post('/sign-up',(req,res,next)=>registerUser(req,res,next))
 router.patch('/verify',(req,res,next)=>verifyUser(req,res,next))
 router.patch('/resend',(req,res,next)=>resendOtp(req,res,next))
 router.post('/login',(req,res,next)=>loginUser(req,res,next))
+router.post('/resetotp',(req,res,next)=>ResetPasswordOtp(req,res,next))
+router.post('/resetpassword',(req,res,next)=>ResetPassword(req,res,next))
 
 export default router
