fab9142489d1b67d681668be42e406866cd0a54e
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-14 10:58:03 +0530

    add the verfiy middleware for route

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -3,12 +3,13 @@ import { uploadImage } from '../controllers/project/uploadData'
 import { getData } from '../controllers/project/getData'
 import { deleteData } from '../controllers/project/deleteData'
 import { editData } from '../controllers/project/editData'
+import { verifyUser } from '../middleware/verifyUser'
 
 const router:Router = express.Router()
 
-router.post('/upload',(req,res,next)=>uploadImage(req,res,next))
-router.get('/getData',(req,res,next)=>getData(req,res,next))
-router.delete('/deleteData/:id/:userid',(req,res,next)=>deleteData(req,res,next))
-router.put('/update',(req,res,next)=>editData(req,res,next))
+router.post('/upload',verifyUser,(req,res,next)=>uploadImage(req,res,next))
+router.get('/getData',verifyUser,(req,res,next)=>getData(req,res,next))
+router.delete('/deleteData/:id/:userid',verifyUser,(req,res,next)=>deleteData(req,res,next))
+router.put('/update',verifyUser,(req,res,next)=>editData(req,res,next))
 
 export default router
