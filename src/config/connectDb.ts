27a2f3ac96e196821b2a7a086847da9b9183f29e
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-17 15:07:19 +0530

    remove the mongouri

--- src/config/connectDb.ts
+++ src/config/connectDb.ts
@@ -3,7 +3,6 @@ import mongoose  from "mongoose";
 
 export const connectToDatabase =  async()=>{
      try {
-          console.log("MongoDB URL:", process.env.MONGO_URI);
           const connectionInstance = await mongoose.connect(process.env.MONGO_ATLAS_URL as string)
           console.log(`\n MogoDB connected ! ! DB HOST: ${connectionInstance.connection.host}`);
      } catch (error:any) {
